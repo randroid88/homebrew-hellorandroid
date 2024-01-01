68f99a4d1d92fcbabffdc9c0b745c002b887217e
Randy Lutcavich <randy.lutcavich@thisisartium.com>
2023-12-31 22:47:34 -0600

    Fixed name and homepage

--- Formula/HelloRandroid.rb
+++ Formula/HelloRandroid.rb
@@ -1,6 +1,6 @@
-class HelloRandroid < Formula
+class Hellorandroid < Formula
   desc "Randroid's Python Hello World script"
-  homepage "https://github.com/randroid88/homebrew-hellowrandroid"
+  homepage "https://github.com/randroid88/homebrew-hellorandroid"
   url "https://github.com/randroid88/homebrew-hellorandroid/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "4cf6eb91735d97e9fced9319e487ea60e0b7d36f87419a4346cbff1a96d42547"
 
