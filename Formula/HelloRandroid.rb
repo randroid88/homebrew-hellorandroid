22e8dcee1942e1b4aa07f3525b05f69064b04a12
Randy Lutcavich <randy.lutcavich@thisisartium.com>
2023-12-31 22:32:47 -0600

    Updated url and sha

--- Formula/HelloRandroid.rb
+++ Formula/HelloRandroid.rb
@@ -1,8 +1,8 @@
 class HelloRandroid < Formula
   desc "Randroid's Python Hello World script"
   homepage "https://github.com/randroid88/homebrew-hellowrandroid"
-  url "https://github.com/randroid88/homebrew-hellowrandroid/archive/v1.0.0.tar.gz"
-  sha256 "<sha-256-of-tar-file>"
+  url "https://github.com/randroid88/homebrew-hellorandroid/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "4cf6eb91735d97e9fced9319e487ea60e0b7d36f87419a4346cbff1a96d42547"
 
   def install
     bin.install "hellorandroid"
